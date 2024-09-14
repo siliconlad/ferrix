a69cf605d868c7d476a7a28398e73ae19ee9daa1
Angus Stewart <siliconlad@protonmail.com>
2024-09-14 22:52:53 +0100

    Pull out setup code for benchmarks

--- benches/mul_benchmark.rs
+++ benches/mul_benchmark.rs
@@ -6,47 +6,42 @@ fn mul_matrix_benchmark(c: &mut Criterion) {
 
     // RowVector * Vector
     group.bench_function("RowVector * Vector", |b| {
-        b.iter(|| {
-            let v1 = RowVector::<i32, 100>::random();
-            let v2 = Vector::<i32, 100>::random();
-            v1 * v2
-        });
+        b.iter_with_setup(
+            || (RowVector::<i32, 100>::random(), Vector::<i32, 100>::random()),
+            |(v1, v2)| v1 * v2
+        );
     });
     
     // Vector * RowVector
     group.bench_function("Vector * RowVector", |b| {
-        b.iter(|| {
-            let v1 = Vector::<i32, 100>::random();
-            let v2 = RowVector::<i32, 100>::random();
-            v1 * v2
-        });
+        b.iter_with_setup(
+            || (Vector::<i32, 100>::random(), RowVector::<i32, 100>::random()),
+            |(v1, v2)| v1 * v2
+        );
     });
 
     // RowVector * Matrix
     group.bench_function("RowVector * Matrix", |b| {
-        b.iter(|| {
-            let v1 = RowVector::<i32, 100>::random();
-            let m1 = Matrix::<i32, 100, 100>::random();
-            v1 * m1
-        });
+        b.iter_with_setup(
+            || (RowVector::<i32, 100>::random(), Matrix::<i32, 100, 100>::random()),
+            |(v1, m1)| v1 * m1
+        );
     });
     
     // Matrix * Vector
     group.bench_function("Matrix * Vector", |b| {
-        b.iter(|| {
-            let m1 = Matrix::<i32, 100, 100>::random();
-            let v1 = Vector::<i32, 100>::random();
-            m1 * v1
-        });
+        b.iter_with_setup(
+            || (Matrix::<i32, 100, 100>::random(), Vector::<i32, 100>::random()),
+            |(m1, v1)| m1 * v1
+        );
     });
 
     // Matrix * Matrix
     group.bench_function("Matrix * Matrix", |b| {
-        b.iter(|| {
-            let m1 = Matrix::<i32, 100, 100>::random();
-            let m2 = Matrix::<i32, 100, 100>::random();
-            m1 * m2
-        });
+        b.iter_with_setup(
+            || (Matrix::<i32, 100, 100>::random(), Matrix::<i32, 100, 100>::random()),
+            |(m1, m2)| m1 * m2
+        );
     });
 }
 
@@ -55,18 +50,18 @@ fn mul_scalar_benchmark(c: &mut Criterion) {
 
     // Vector * Scalar
     group.bench_function("Vector * Scalar", |b| {
-        b.iter(|| {
-            let v1 = Vector::<i32, 100>::random();
-            v1 * 1
-        });
+        b.iter_with_setup(
+            || Vector::<i32, 100>::random(),
+            |v1| v1 * black_box(2)
+        );
     });
 
     // Matrix * Scalar
     group.bench_function("Matrix * Scalar", |b| {
-        b.iter(|| {
-            let m1 = Matrix::<i32, 100, 100>::random();
-            m1 * 1
-        });
+        b.iter_with_setup(
+            || Matrix::<i32, 100, 100>::random(),
+            |m1| m1 * black_box(2)
+        );
     });
 }
 
@@ -75,18 +70,18 @@ fn mul_scalar_assign_benchmark(c: &mut Criterion) {
 
     // Vector *= Scalar
     group.bench_function("Vector *= Scalar", |b| {
-        b.iter(|| {
-            let mut v1 = Vector::<i32, 100>::random();
-            v1 *= black_box(1)
-        });
+        b.iter_with_setup(
+            || Vector::<i32, 100>::random(),
+            |mut v1| v1 *= black_box(2)
+        );
     });
 
     // Matrix *= Scalar
     group.bench_function("Matrix *= Scalar", |b| {
-        b.iter(|| {
-            let mut m1 = Matrix::<i32, 100, 100>::random();
-            m1 *= black_box(1)
-        });
+        b.iter_with_setup(
+            || Matrix::<i32, 100, 100>::random(),
+            |mut m1| m1 *= black_box(2)
+        );
     });
 }
 
